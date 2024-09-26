64dcc6a367d83ed408e07dc9c28554f472fe217f
Lucas Viana <vianalucas812@gmail.com>
2024-09-26 15:55:54 -0300

    Feat: Configurando classe pessoa

--- accounts/services/pessoa.py
+++ accounts/services/pessoa.py
@@ -1,66 +1,10 @@
+from abc import ABC, abstractmethod
 
-class Pessoa:
+class Pessoa(ABC):
     
     def __init__(self, conexao):
         self.conexao = conexao
-
-
-    def conta_tipo_funcionario(self, tipos, data_fim, codigo_empresa):
-        where_clause = f"TIPO in {tipos} AND DATACADASTRO <= '{data_fim}'"
-        if codigo_empresa:
-            where_clause += f" AND CODIGOEMPRESA = {codigo_empresa}"
-
-        data = (
-            self.conexao.select('TABPESSOAS')
-            .where(where_clause)
-            .values("CASE WHEN TIPO = 'O' THEN 'Pró Labore' WHEN TIPO = 'N' THEN 'Funcionários CLT' WHEN TIPO = 'M' THEN 'Autônomos' END AS TIPO, COUNT(CODIGO)")
-            .group_by("TIPO")
-            .execute()
-        )
-
-        return dict((item[0].strip(), item[1]) for item in data) if data else {}
-
-
-    def contar_estagiarios(self, data_fim, codigo_empresa):
-        where_clause = f"ESTAGIARIO = 1 AND TIPO = 'O' AND DATACADASTRO <= '{data_fim}'"
-        if codigo_empresa:
-            where_clause += f" AND CODIGOEMPRESA = {codigo_empresa}"
-
-        data = (
-            self.conexao.select('TABPESSOAS')
-            .where(where_clause)
-            .values("COUNT(CODIGO)")
-            .execute()
-        )
-
-        return data[0][0] if data else 0
-
-
-    def contar_admissoes(self, data_ini, data_fim, codigo_empresa):
-        where_clause = f"DATACADASTRO BETWEEN '{data_ini}' AND '{data_fim}' AND TIPO = 'N'"
-        if codigo_empresa:
-            where_clause += f" AND CODIGOEMPRESA = {codigo_empresa}"
-
-        data = (
-            self.conexao.select('TABPESSOAS')
-            .where(where_clause)
-            .values("COUNT(CODIGO)")
-            .execute()
-        )
-
-        return data[0][0] if data else 0
-
-
-    def contar_demissoes(self, data_ini, data_fim, codigo_empresa):
-        where_clause = f"DATADEMISSAO BETWEEN '{data_ini}' AND '{data_fim}' AND TIPO = 'N'"
-        if codigo_empresa:
-            where_clause += f" AND CODIGOEMPRESA = {codigo_empresa}"
-
-        data = (
-            self.conexao.select('TABPESSOAS')
-            .where(where_clause)
-            .values("COUNT(CODIGO)")
-            .execute()
-        )
-
-        return data[0][0] if data else 0
+        
+    @abstractmethod
+    def lista_cadastros(self):
+        pass
