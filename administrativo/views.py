16cef7c2e75c83d3aa39ad836e1affd6c7a734b1
Lucas Viana <vianalucas812@gmail.com>
2024-09-27 09:23:41 -0300

    fix: aplicando correção temporaria para apresentação

--- administrativo/views.py
+++ administrativo/views.py
@@ -15,21 +15,47 @@ def retorna_volumetria_consumo_cliente(request):
     clientes = ClienteService(conexao)    
     lista_clientes = clientes.obter_clientes_formatados_formulario()
     
-    cliente = request.POST.get("cliente")
-    pick_realizado = converte_array_data_para_sistema(request.POST.get("pick_realizado").split(" - "))
-    pick_comparativo = converte_array_data_para_sistema(request.POST.get("pick_comparativo").split(" - "))
-    demonstracao = request.POST.get("demonstracao")
-    descendentes = request.POST.get("descendentes")   
     
-    data_ini = pick_realizado[0]
-    data_fim = pick_realizado[1]
+    if request.POST:
     
-    data_comparativo_ini = pick_comparativo[0]
-    data_comparativo_fim = pick_comparativo[1]
-    codigo_empresa = []
+        cliente = request.POST.get("cliente")
+        pick_realizado = converte_array_data_para_sistema(request.POST.get("pick_realizado").split(" - "))
+        pick_comparativo = converte_array_data_para_sistema(request.POST.get("pick_comparativo").split(" - "))
+        demonstracao = request.POST.get("demonstracao")
+        descendentes = request.POST.get("descendentes") 
+        
+        filtros = {
+            "pick_realizado": request.POST.get("pick_realizado"),
+            "pick_comparativo": request.POST.get("pick_comparativo")
+        }  
+        
+        data_ini = pick_realizado[0]
+        data_fim = pick_realizado[1]
+        
+        data_comparativo_ini = pick_comparativo[0]
+        data_comparativo_fim = pick_comparativo[1]
+        codigo_empresa = []
+    else:
+        filtros = {
+            "pick_realizado": ['11/09/2024 - 10/10/2024'],
+            "pick_comparativo": ['11/08/2024 - 10/09/2024']
+        }  
+        
+        data_ini = "2024-09-11"
+        data_fim = "2024-10-10"
+        
+        data_comparativo_ini = "2024-08-11"
+        data_comparativo_fim = "2024-09-10"
+        codigo_empresa = []
+            
+    
     
     # Volumetria 
     volumetria = VolumetriaAthenas(conexao, codigo_empresa = codigo_empresa, data_ini = data_ini, data_fim = data_fim, data_comparativo_ini = data_comparativo_ini, data_comparativo_fim = data_comparativo_fim)    
     data = volumetria.controllerMetricas()
     
-    return render(request, 'dashboards/volumetria_cliente.html', {"clientes": lista_clientes, "data": data})
+    return render(request, 'dashboards/volumetria_cliente.html', {"filtros": filtros, "clientes": lista_clientes, "data": data})
+
+
+def filtro_volumetria(request):
+    pass
