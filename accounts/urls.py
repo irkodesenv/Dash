80e32bb22c288ece0b107352e1467da23129eb45
Lucas Viana <vianalucas812@gmail.com>
2024-10-10 09:54:25 -0300

    Feat: implementando login via AD e não mais pelo athenas

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('accounts/login/', views.login, name='login'),
+    path('', views.auth_login, name='login'),
+    path('accounts/login/', views.auth_login, name='login'),
     path('logoff/', views.logoff, name='logoff'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-from .services.usuario import Usuario
 from django.contrib import auth
+import requests
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
 
-def login(request):
+def auth_login(request):
 
     if request.user.is_authenticated:
         return redirect('dashboard:dashboard')
@@ -12,20 +14,49 @@ def login(request):
     if request.method != 'POST':
         return render(request, 'accounts/login.html')
     
+    
     usuario = request.POST.get('usuario', "").strip()
     senha = request.POST.get('senha', "")
+    
+    
+    url = 'http://10.11.100.122:8002/api/login/'
+    
+    data = {
+        'username': usuario,
+        'password': senha
+    }
+    
+    response = requests.post(url, data=data)
+    response_data = response.json()
+    nome = response_data.get("first_name")
+    email = response_data.get("email")
 
 
-    usuario = Usuario(usuario = usuario, senha = senha)
-    usuario_autenticado = usuario.authenticate()
+    if response.status_code == 200:
+        # Autenticar o usuário com o modelo padrão do Django
+        user = authenticate(username=usuario, password=senha)
 
+        if user:
+            login(request, user)
+            return redirect('dashboard:dashboard')
+        else:
+            user = User.objects.create_user(
+                username=usuario,
+                password=senha,
+                first_name=nome,
+                email=email,
+                is_active=True
+            )
+            # Autenticar o novo usuário
+            user_auth = authenticate(username=usuario, password=senha)
 
-    if usuario_autenticado:
-        auth.login(request, usuario_autenticado)
-        return redirect('dashboard:dashboard')
-    else:
-        return render(request, 'accounts/login.html', {'error': 'Usuário ou senha inválidos'})
+            if user_auth:
+                login(request, user_auth)
+                return redirect('dashboard:dashboard')
 
+    if response.status_code == 400 or response.status_code == 401:
+        return render(request, 'accounts/login.html', {'error': 'Usuário ou senha inválidos'})    
+    
 
 def logoff(request):
     auth.logout(request)
